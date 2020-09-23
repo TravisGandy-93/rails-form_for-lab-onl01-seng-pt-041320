82b4a2b6108604b49e71a43ec1b55b6246f6b106
Travis Gandy <travis_gandy@yahoo.com>
2020-09-23 16:29:54 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,15 +12,28 @@ class SchoolClassesController < ApplicationController
 	end
 	
 	def create
-		@school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+		@school_class = SchoolClass.new(post_params(:title, :room_number))
+		@school_class.save
 		redirect_to school_class_path(@school_class)
 	end
 
 	def update
 		@school_class = SchoolClass.find(params[:id])
-		@school_class.update(params.require(:school_class))
-		@school_class.save
+		@school_class.update(post_params(:title, :room_number))
 		redirect_to school_class_path(@school_class)
 	end
+
+	
+
+	private
+ 
+ 
+	# We pass the permitted fields in as *args;
+	# this keeps `post_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action
+		def post_params(*args)
+		  params.require(:school_class).permit(*args)
+		end
 
 end
